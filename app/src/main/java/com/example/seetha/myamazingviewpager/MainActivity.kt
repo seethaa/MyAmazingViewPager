ce0867f082471a76438ceb43ad774e4e039af6a4
seethaa <seetha.annamraju@gmail.com>
2018-08-17 06:25:11 -0700

    ch 2: add tabs to your viewpager

--- app/src/main/java/com/example/seetha/myamazingviewpager/MainActivity.kt
+++ app/src/main/java/com/example/seetha/myamazingviewpager/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.seetha.myamazingviewpager
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +16,17 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         // Get ViewPager from layout
+        val pager = findViewById<ViewPager>(R.id.activity_main_viewpager)
+
         // Set PageAdapter as adapter for viewpager
-        activity_main_viewpager.adapter = PageAdapter(supportFragmentManager, resources.getIntArray(R.array.colorPagesViewPager))
+        pager.adapter = PageAdapter(supportFragmentManager)
+
+        // Get Tablayout and set up with the viewpager
+        activity_main_tabs.setupWithViewPager(pager)
+
+        // Enable tabs to have the same width
+        activity_main_tabs.tabMode = TabLayout.MODE_FIXED
 
     }
-
 
 }
--- app/src/main/java/com/example/seetha/myamazingviewpager/PageAdapter.kt
+++ app/src/main/java/com/example/seetha/myamazingviewpager/PageAdapter.kt
@@ -4,15 +4,25 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 
-open class PageAdapter(manager: FragmentManager,
-                       private val colors: IntArray) : FragmentPagerAdapter(manager) {
+open class PageAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
 
     override fun getCount(): Int {
-        return colors.size // Number of pages to show
+        return 3 // Number of pages to show
     }
 
-    override fun getItem(position: Int): Fragment {
-        //Page to return
-        return PageFragment.newInstance(position, this.colors[position])
-    }
+    override fun getItem(position: Int): Fragment =
+            when (position) {
+                0 -> NewsFeedFragment.newInstance() //Page 0
+                1 -> ProfilePageFragment.newInstance() //Page 1
+                2 -> SettingsFragment.newInstance() //Page 2
+                else -> PageFragment.newInstance(0, 0)
+            }
+
+    override fun getPageTitle(position: Int): CharSequence? =
+            when (position) {
+                0 -> "News Feed"
+                1 -> "Profile"
+                2 -> "Settings"
+                else -> "Default page"
+            }
 }
